fb625fec8914d593700805fb27e8d1058cc45b59
dragoshomner <dragoshomner@yahoo.com>
2023-05-06 19:46:48 +0200

    Updates

---
+++ precache-manifest.846c621f68fafa6e118124bc41d60ce9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c448316db476fe5dbeb",
+    "url": "/modbd-fe/static/css/main.d39c3c0c.chunk.css"
+  },
+  {
+    "revision": "2c448316db476fe5dbeb",
+    "url": "/modbd-fe/static/js/main.0304887f.chunk.js"
+  },
+  {
+    "revision": "4ff117cb759cee7d6d21",
+    "url": "/modbd-fe/static/js/runtime~main.773ebc55.js"
+  },
+  {
+    "revision": "2fcbcd5e522736857dcb",
+    "url": "/modbd-fe/static/js/2.95dabda9.chunk.js"
+  },
+  {
+    "revision": "adde51e780694491e38e",
+    "url": "/modbd-fe/static/js/3.94bb64a9.chunk.js"
+  },
+  {
+    "revision": "8a0896a8d781d46d46dc720deed6c581",
+    "url": "/modbd-fe/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.37b39bbedc8b49419e8f4a5f198fc4c2.js"
+  "/modbd-fe/precache-manifest.846c621f68fafa6e118124bc41d60ce9.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/modbd-fe/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
