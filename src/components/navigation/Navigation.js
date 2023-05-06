6a4626cbd8c5220d8cf3512442b37f3269de1506
dragoshomner <dragoshomner@yahoo.com>
2023-05-06 16:25:12 +0200

    add all tabs

--- src/components/navigation/Navigation.js
+++ src/components/navigation/Navigation.js
@@ -7,6 +7,12 @@ import {
   EMPLOYEES_TAB, 
   HOTELS_TAB, 
   JOBS_TAB, 
+  ROOMS_TAB,
+  RESERVATIONS_TAB,
+  CLIENTS_TAB,
+  ADDRESSES_TAB,
+  CITIES_TAB,
+  COUNTRIES_TAB,
   currentTab 
 } from './tabs';
   
@@ -24,6 +30,12 @@ export function Navigation() {
           <LinkTab label="Hotels" to={`/${HOTELS_TAB}`} />
           <LinkTab label="Employees" to={`/${EMPLOYEES_TAB}`} />
           <LinkTab label="Jobs" to={`/${JOBS_TAB}`} />
+          <LinkTab label="Rooms" to={`/${ROOMS_TAB}`} />
+          <LinkTab label="Reservations" to={`/${RESERVATIONS_TAB}`} />
+          <LinkTab label="Clients" to={`/${CLIENTS_TAB}`} />
+          <LinkTab label="Addresses" to={`/${ADDRESSES_TAB}`} />
+          <LinkTab label="Cities" to={`/${CITIES_TAB}`} />
+          <LinkTab label="Countries" to={`/${COUNTRIES_TAB}`} />
         </Tabs>
       </Box>
       <Outlet />
--- src/components/navigation/tabs.js
+++ src/components/navigation/tabs.js
@@ -1,11 +1,23 @@
 export const HOTELS_TAB = 'hotels';
 export const EMPLOYEES_TAB = 'employees';
 export const JOBS_TAB = 'jobs';
+export const ROOMS_TAB = 'rooms';
+export const RESERVATIONS_TAB = 'reservations';
+export const CLIENTS_TAB = 'clients';
+export const ADDRESSES_TAB = 'addresses';
+export const CITIES_TAB = 'cities';
+export const COUNTRIES_TAB = 'countries';
 
 const TABS_MAPPING = {
   [HOTELS_TAB]: 0,
   [EMPLOYEES_TAB]: 1,
   [JOBS_TAB]: 2,
+  [ROOMS_TAB]: 3,
+  [RESERVATIONS_TAB]: 4,
+  [CLIENTS_TAB]: 5,
+  [ADDRESSES_TAB]: 6,
+  [CITIES_TAB]: 7,
+  [COUNTRIES_TAB]: 8,
 };
 
 export const currentTab = () => {
